319a5de2481d0c05a4f7aab630a29b492d4ce321
Noel Manuel <setsuna04@gmail.com>
2018-02-12 01:22:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function johnLennonFacts(fact) {
   }
   return facts;
 }
+
+function iLoveTheBeatles(num) {
+  var beatles = [];
+  do {
+    beatles.push('I love the Beatles!');
+    num--;
+  } while (num >= 0)
+}
