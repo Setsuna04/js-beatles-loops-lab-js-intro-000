9bb832c5ac5fbb282bc8728614f90170cc5408c9
Noel Manuel <setsuna04@gmail.com>
2018-02-12 01:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instrument) {
   var sentence = [];
-  for (i = 0; i < musicians.length(); i++) {
+  for (let i = 0; i < musicians.length(); i++) {
     sentence.push(musicians + instrument);
   }
   return sentence;
